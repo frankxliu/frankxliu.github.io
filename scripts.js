84ae19ba82d9aefa3f33920431e7acb2a2349879
frankxliu <frankieliu.23@gmail.com>
2020-08-21 02:45:55 -0400

    added scripts

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("a1").onclick = function(){
+    document.getElementById('content').scrollIntoView(true);
+}
+
+document.getElementById("a2").onclick = function(){
+    document.getElementById('footer').scrollIntoView(true);
+}
