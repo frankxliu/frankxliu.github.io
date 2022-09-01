ce6b82659d114bd95202f6906dd7b0e45f72af8b
Frankie Liu <frankieliu.23@gmail.com>
2022-09-01 17:46:52 -0400

    delete old js file

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("a1").onclick = function(){
-    document.getElementById('content').scrollIntoView(true);
-}
-
-document.getElementById("a2").onclick = function(){
-    document.getElementById('footer').scrollIntoView(true);
-}
